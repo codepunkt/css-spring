8ebd5b46c819f4fa097237856d239a51225c3e20
Christoph Werner <christoph+github@codepunkt.de>
2017-02-07 16:07:59 +0100

    Add webpack builds

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack')
+var compact = require('lodash').compact
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+
+var minify = process.env.MINIFY_BUILD
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: `dist/css-spring${minify ? '.min' : ''}.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            [ 'es2015', { modules: false }],
+          ],
+        },
+      },
+    ],
+  },
+  plugins: compact([
+    new LodashModuleReplacementPlugin(),
+    minify ? new webpack.optimize.UglifyJsPlugin() : null,
+  ]),
+}
